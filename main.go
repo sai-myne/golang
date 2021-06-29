a43cd5d4eea62c4ac938ca7bd84cd85bc5dac99f
sai-myne <ahlusir@gmail.com>
2021-06-29 18:49:58 +0800

    lesson-4

--- main.go
+++ main.go
@@ -3,31 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	// strings
-	// var nameOne string = "mario"
-	// var nameTwo = "luigi"
-	// var nameThree string
-	// fmt.Println(nameOne, nameTwo, nameThree)
-	// nameOne = "peach"
-	// nameThree = "bowser"
-	// fmt.Println(nameOne, nameTwo, nameThree)
-	// nameFour := "yoshi"
-	// fmt.Println((nameFour))
+	age := 35
+	name := "shaun"
 
-	// ints
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
+	// Print
+	fmt.Print("hello, ")
+	fmt.Print("world! \n")
+	fmt.Print("new line \n")
 
-	fmt.Println(ageOne, ageTwo, ageThree)
+	fmt.Println("hello ninjas!")
+	fmt.Println("goodbyle ninjas!")
+	fmt.Println("my age is", age, "and my name is", name)
 
-	// bits & memory
-	// var numOne int8 = 25
-	// var numTwo int8 = -128
-	// var numThree uint16 = 256
+	// Printf(formatted stings)
+	fmt.Printf("my age is %v and my name is %v \n", age, name)
+	fmt.Printf("my age is %q and my name is %q \n", age, name)
+	fmt.Printf("age is of type %T \n", age)
+	fmt.Printf("you scored %f points! \n", 225.55)
+	fmt.Printf("you scored %0.1f points! \n", 225.55)
 
-	var scoreOne float32 = 25.98
-	var scoreTwo float64 = 343231423142341234.32412412341234
-	scoreThree := 1.5
+	// Sprintf(save formatted strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
+	fmt.Println("the saved string is", str)
 
 }
