9bfceeab6ce375df3d75b3d174d2d53fa0113a60
sai-myne <ahlusir@gmail.com>
2021-06-29 18:37:07 +0800

    lesson-3

--- main.go
+++ main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, ninjas!")
+	// strings
+	// var nameOne string = "mario"
+	// var nameTwo = "luigi"
+	// var nameThree string
+	// fmt.Println(nameOne, nameTwo, nameThree)
+	// nameOne = "peach"
+	// nameThree = "bowser"
+	// fmt.Println(nameOne, nameTwo, nameThree)
+	// nameFour := "yoshi"
+	// fmt.Println((nameFour))
+
+	// ints
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits & memory
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+	// var numThree uint16 = 256
+
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 343231423142341234.32412412341234
+	scoreThree := 1.5
+
 }
