bcbab063b1e7966f1fee5d632249060a33bf0c21
sai-myne <ahlusir@gmail.com>
2021-06-29 19:31:03 +0800

    Booleans & Conditionals

--- main.go
+++ main.go
@@ -5,31 +5,34 @@ import (
 )
 
 func main() {
-	// x := 0
-	// for x < 5 {
-	// 	fmt.Println("value of x is:", x)
-	// 	x++
+	// age := 25
+
+	// fmt.Println(age <= 50)
+	// fmt.Println(age >= 50)
+	// fmt.Println(age == 50)
+	// fmt.Println(age != 50)
+
+	// if age < 30 {
+	// 	fmt.Println("age is less than 30")
+	// } else if age < 40 {
+	// 	fmt.Println("age is less than 40")
+	// } else {
+	// 	fmt.Println("age is not less than 45")
 	// }
 
-	// for i := 0; i < 5; i++ {
-	// 	fmt.Println("value of i is:", i)
-	// }
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
 
-	names := []string{"mario", "luigi", "yoshi", "peach"}
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("breaking at pos", index)
+			break
+		}
 
-	// for i := 0; i < len(names); i++ {
-	// 	fmt.Println(names[i])
-	// }
-
-	// for index, value := range names {
-	// 	fmt.Printf("the value at index %v is %v \n", index, value)
-	// }
-
-	for _, value := range names {
-		fmt.Printf("the value at is %v \n", value)
-		value = "new string"
+		fmt.Printf("the value at pos %v is %v \n", index, value)
 	}
-
-	fmt.Println(names)
 
 }
