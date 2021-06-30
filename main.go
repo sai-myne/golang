d0ddd6c5f3135bd82684e685acaea94bd1d83155
sai-myne <ahlusir@gmail.com>
2021-06-30 18:52:13 +0800

    Package Scope

--- main.go
+++ main.go
@@ -1,34 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
-
-	var initials []string
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-
-	return initials[0], "_"
-}
+var score = 99.5
 
 func main() {
-	fn1, sn1 := getInitials("tifa lockhart")
-	fmt.Println(fn1, sn1)
-
-	fn2, sn2 := getInitials("cloud strife")
-	fmt.Println(fn2, sn2)
-
-	fn3, sn3 := getInitials("barret")
-	fmt.Println(fn3, sn3)
-
+	sayHello("mario")
+	for _, v := range points {
+		fmt.Println(v)
+	}
+	showScore()
 }
