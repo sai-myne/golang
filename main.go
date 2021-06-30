c28ac12c9403ea9905e22146003aa83b6e8a49d8
sai-myne <ahlusir@gmail.com>
2021-06-30 19:20:02 +0800

    Pointers

--- main.go
+++ main.go
@@ -2,27 +2,19 @@ package main
 
 import "fmt"
 
-func updateName(x string) string {
-	x = "wedge"
-	return x
-}
-
-func updateMenu(y map[string]float64) {
-	y["coffee"] = 2.99
+func updateName(x *string) {
+	*x = "wedge"
 }
 
 func main() {
-	// group A types -> strings, ints, bools, floats, arrays, structs
 	name := "tifa"
 
-	name = updateName(name)
+	// updateName(name)
+	// fmt.Println("memory address of name is:", &name)
+	m := &name
+	// fmt.Println("memory address:", m)
+	// fmt.Println("value at memory address:", *m)
 	fmt.Println(name)
-
-	// group B types -> slices, maps, functions
-	menu := map[string]float64{
-		"pie":       5.95,
-		"ice cream": 3.99,
-	}
-	updateMenu(menu)
-	fmt.Println(menu)
+	updateName(m)
+	fmt.Println(name)
 }
