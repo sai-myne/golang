f0265ae70b32be097f8a9207ed7b4703cf5e8aa4
sai-myne <ahlusir@gmail.com>
2021-06-30 18:58:53 +0800

    Maps

--- main.go
+++ main.go
@@ -2,12 +2,34 @@ package main
 
 import "fmt"
 
-var score = 99.5
-
 func main() {
-	sayHello("mario")
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"soup":           4.99,
+		"pie":            7.99,
+		"salad":          6.99,
+		"toffee pudding": 3.55,
 	}
-	showScore()
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	// loop maps
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// ints as key type
+	phonebook := map[int]string{
+		23242343: "mario",
+		12332434: "luigi",
+		56463565: "peach",
+	}
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[23242343])
+
+	phonebook[12332434] = "bowser"
+	fmt.Println(phonebook)
+
+	phonebook[56463565] = "yoshi"
+	fmt.Println(phonebook)
+
 }
